5482779765f7835e16ba887632ea4d5edf3d8311
KamQuoss <51073261+KamQuoss@users.noreply.github.com>
2021-12-11 05:58:09 -0800

    initial commit

--- src/FormFunction.js
+++ src/FormFunction.js
@@ -4,24 +4,25 @@ const Form = () => {
   const tipOptions = [5, 10, 15, 20];
   const taxValue = 1.05;
 
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(null);
   const [tip, setTip] = useState(0);
-  const [toPay, setToPay] = useState(0);
+  const [result, setResult] = useState(0);
 
   const onAmountChange = (e) => setAmount(e.target.value);
   const onTipChange = (e) => setTip(Number(e.target.value));
 
   const handleClick = (e) => {
     e.preventDefault();
-    let toPay = (tip ? amount * ((100 + tip) / 100) : amount) * taxValue;
-    setToPay(toPay);
+    !amount
+      ? alert("Amount is missing")
+      : setResult((tip ? amount * ((100 + tip) / 100) : amount) * taxValue);
   };
 
   return (
     <div>
-      {toPay === 0 && (
+      {result === 0 && (
         <form>
-          <label htmlFor="amount">Net mount: </label>
+          <label htmlFor="amount">Net amount: </label>
           <input
             type="number"
             min="0"
@@ -46,9 +47,9 @@ const Form = () => {
           </button>
         </form>
       )}
-      {toPay !== 0 && (
+      {result !== 0 && (
         <p>
-          gross amount: <span>{toPay.toFixed(2)}</span>
+          gross amount: <span>{result.toFixed(2)}</span>
         </p>
       )}
     </div>
